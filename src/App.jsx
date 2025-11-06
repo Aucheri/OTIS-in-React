5579e1d6c53188c926b810b3f32ed4ee408b4b1a
liamg2810 <lgardn3r@gmail.com>
2025-11-06 16:24:21 +0000

    bolding

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
 		<div className="h-screen w-screen bg-neutral-50">
 			<div className="absolute mx-2 my-4 flex items-center gap-4">
 				<img src={Otis} className="w-12" alt="Otis Logo" />
-				<h1 className="hidden text-3xl sm:block">Otis</h1>
+				<h1 className="hidden text-3xl sm:block font-bold">Otis</h1>
 			</div>
 
 			<div className="h-screen lg:left-1/2 lg:w-1/2 lg:translate-x-1/2">
